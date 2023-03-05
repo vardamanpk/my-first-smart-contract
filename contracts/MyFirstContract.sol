0ef6ff41a5069c47f3ca94a128a5b4c51474eb6b
Vardaman PK <vardamanpk@gmail.com>
2023-03-06 02:17:53 +0530

    Define MyFirstContract

---
+++ contracts/MyFirstContract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyFirstContract {
+    string private data;
+
+    function setData(string memory _data) public {
+        data = _data;
+    }
+
+    function getData() public view returns (string memory) {
+        return data;
+    }
+
+}
